46e23dfe659411191f3962a00bb51364483ffbad
Wing-Ho Ko <wingho@uw.edu>
2026-03-17 14:23:10 -0700

    Added install.R file

---
+++ install.R
@@ -0,0 +1,8 @@
+install.packages(c(
+  "devtools", "tidyverse", "patchwork", "janitor", "rmarkdown",
+  "knitr", "writexl", "svglite", "rgl", "pkgdown",
+  "boot", "car", "carData", "pwr", "effectsize",
+  "vegan", "umap", "ggdendro", "lmtest", "statmod",
+  "lme4", "nlme", "mgcv", "VGAM", "ciTools",
+  "ggbiplot", "ecotox", "varTestnlme", "gratia", "palmerpenguins"
+))
