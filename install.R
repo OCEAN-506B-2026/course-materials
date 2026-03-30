be78b91ad7b812b99d80b8d5e4335280c47b8d2f
Wing-Ho Ko <wingho@uw.edu>
2026-03-29 20:13:42 -0700

    Modified install.R

--- install.R
+++ install.R
@@ -2,7 +2,8 @@ install.packages(c(
   "devtools", "tidyverse", "patchwork", "janitor", "rmarkdown",
   "knitr", "writexl", "svglite", "rgl", "pkgdown",
   "boot", "car", "carData", "pwr", "effectsize",
-  "vegan", "umap", "ggdendro", "lmtest", "statmod",
+  "factominer", "factoextra", "cluster", "dbscan", "ggbiplot",
+  "ggdendro", "vegan", "umap", "lmtest", "statmod",
   "lme4", "nlme", "mgcv", "VGAM", "ciTools",
-  "ggbiplot", "ecotox", "varTestnlme", "gratia", "palmerpenguins"
+  "ecotox", "varTestnlme", "gratia", "palmerpenguins", "ncdf4"
 ))
