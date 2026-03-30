d4566a690488cc4da78afc73eb40eb830bf02534
Wing-Ho Ko <wingho@uw.edu>
2026-03-30 09:54:07 -0700

    Corrected typo in install.R

--- install.R
+++ install.R
@@ -2,7 +2,7 @@ install.packages(c(
   "devtools", "tidyverse", "patchwork", "janitor", "rmarkdown",
   "knitr", "writexl", "svglite", "rgl", "pkgdown",
   "boot", "car", "carData", "pwr", "effectsize",
-  "factominer", "factoextra", "cluster", "dbscan", "ggbiplot",
+  "FactoMineR", "factoextra", "cluster", "dbscan", "ggbiplot",
   "ggdendro", "vegan", "umap", "lmtest", "statmod",
   "lme4", "nlme", "mgcv", "VGAM", "ciTools",
   "ecotox", "varTestnlme", "gratia", "palmerpenguins", "ncdf4"
