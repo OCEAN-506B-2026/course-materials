e6f3969b212a7bf09b3bdf94e201dbdd446fa2e3
Wing-Ho Ko <wingho@uw.edu>
2026-04-02 16:00:57 -0700

    Added more packages to install.R

--- install.R
+++ install.R
@@ -1,10 +1,10 @@
 install.packages(c(
   "devtools", "tidyverse", "patchwork", "janitor", "rmarkdown",
   "knitr", "writexl", "svglite", "rgl", "pkgdown",
-  "boot", "car", "carData", "pwr", "effectsize",
-  "FactoMineR", "factoextra", "cluster", "dbscan", "ggbiplot",
-  "ggdendro", "vegan", "umap", "broom", "lmtest", 
-  "statmod", "ARTool", "lme4", "nlme", "mgcv", 
-  "VGAM", "ciTools", "ecotox", "varTestnlme", "gratia", 
-  "palmerpenguins", "ncdf4"
+  "boot", "car", "carData", "pwr", "MKpower", 
+  "effectsize", "e1071", "FactoMineR", "factoextra", "cluster", 
+  "dbscan", "ggbiplot","ggdendro", "vegan", "umap", 
+  "broom", "lmtest", "statmod", "ARTool", "lme4", 
+  "nlme", "broom.mixed", "mgcv", "VGAM", "ciTools", 
+  "ecotox", "varTestnlme", "gratia", "palmerpenguins", "ncdf4"
 ))
